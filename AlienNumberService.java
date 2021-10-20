c38d9e989cc9689245b520c96d3786b5be9e2226
Atul J <92841927+Atul-Jalal@users.noreply.github.com>
2021-10-20 21:49:36 +0530

    Add files via upload

---
+++ AlienNumberService.java
@@ -0,0 +1,69 @@
+package com.litmus7.training.alien;
+
+/**
+ * @author avinash.panicker The service class for the Alien Number system
+ * some edit by sangeeth
+ */
+public class AlienNumberService {
+
+	/**
+	 * ORGBASE is the original base of the string passed. for decimal 10
+	 */
+	private final static int ORGBASE = 10;
+	/**
+	 * ALIENBASE is the base of the alien number system, here 3
+	 */
+	private final static int ALIENBASE = 3;
+
+	/**
+	 * method where base conversion to the Alien number system happens.
+	 * 
+	 * @param number is the number received as a string
+	 * @return the function returns a string builder as the alien number.
+	 */
+	public String toAlien(final String number) {
+		final String temporaryNum = Integer.toString(Integer.parseInt(number, ORGBASE), ALIENBASE);
+		final StringBuilder actualNumber = new StringBuilder();
+
+		for (int i = 0; i < temporaryNum.length(); i++) {
+			switch (temporaryNum.charAt(i)) {
+			case '0':
+				actualNumber.append('0');
+				break;
+			case '1':
+				actualNumber.append('F');
+				break;
+			case '2':
+				actualNumber.append('8');
+				break;
+			}
+		}
+		return actualNumber.toString();
+	}
+
+	/**
+	 * method where base conversion to the Decimal number system happens.
+	 * 
+	 * @param number is the Alien number received as a string
+	 * @return the function returns a string builder as the decimal number.
+	 */
+	public String toDecimal(final String number) {
+		final StringBuilder temporaryNum = new StringBuilder();
+
+		for (int i = 0; i < number.length(); i++) {
+			switch (number.charAt(i)) {
+			case '0':
+				temporaryNum.append('0');
+				break;
+			case 'F':
+				temporaryNum.append('1');
+				break;
+			case '8':
+				temporaryNum.append('2');
+				break;
+			}
+		}
+		final String actualNumber = Integer.toString(Integer.parseInt(temporaryNum.toString(), ALIENBASE), ORGBASE);
+		return actualNumber.toString();
+	}
+}
